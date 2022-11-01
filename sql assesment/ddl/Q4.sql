750825180756ecf2807b7f82c605ced0fc90a58e
Fousen <80414662+Fousen@users.noreply.github.com>
2022-11-01 12:11:48 +0530

    Delete sql assesment directory

--- sql assesment/ddl/Q4.sql
+++
@@ -1,9 +0,0 @@
-use q3;
-
-update t_doctor
-set fee = 350 where specialization = 'ENT' and doctor_name = 'J%';
-
-update t_doctor
-set fee = 600 where specialization = 'Derma' and doctor_name = 'J%';
-
-select*from t_doctor;
